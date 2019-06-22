41c33ed934dca8d4601a9051e9d7387df3bc4081
fiyazbinhasan <fiyazhasan@live.com>
2019-06-22 17:42:54 +0600

    first push

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AddressBookMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddressBookMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        private static List<PersonViewModel> _people;
+        public PersonController()
+        {
+            _people = new List<PersonViewModel>();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(_people);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("FirstName,LastName,Address")] PersonViewModel person)
+        {
+            _people.Add(person);
+            
+            return View("List", _people);
+        }
+    }
+}
---
+++ Models/PersonViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBookMVC.Models
+{
+    public class PersonViewModel
+    {
+        [DisplayName("First Name")]
+        [MaxLength(10)]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+}
